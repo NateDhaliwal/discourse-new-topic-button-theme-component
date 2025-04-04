ac5bb9854c3141528665d87ac7c9039477665994
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-04-04 09:58:08 +0800

    Update and rename initialize-header-new-topic-button.gjs to initialize-header-new-topic-button.js

--- javascripts/discourse/initializers/initialize-header-new-topic-button.js
+++ javascripts/discourse/initializers/initialize-header-new-topic-button.js
@@ -3,4 +3,9 @@ import CustomHeaderTopicButton from "../components/custom-header-topic-button";
 
 export default apiInitializer("1.15.0", (api) => {
   api.renderInOutlet("below-site-header", CustomHeaderTopicButton);
+  const newTopicBtn = document.getElementById("new-topic-button");
+  const sidebar = document.getElementById("d-sidebar");
+  if (newTopicBtn && sidebar) {
+    sidebar.insertBefore(newTopicBtn, sidebar.firstChild);
+  }
 });
