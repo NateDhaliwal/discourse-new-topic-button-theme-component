0b7f5b19294f8d26f306f77fa9ac81dfe2efa5d2
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-04-04 10:54:58 +0800

    Update initialize-header-new-topic-button.js

--- javascripts/discourse/initializers/initialize-header-new-topic-button.js
+++ javascripts/discourse/initializers/initialize-header-new-topic-button.js
@@ -2,8 +2,8 @@ import { apiInitializer } from "discourse/lib/api";
 import CustomHeaderTopicButton from "../components/custom-header-topic-button";
 
 export default apiInitializer((api) => {
-  api.renderInOutlet("below-site-header", CustomHeaderTopicButton);
-  
+  api.renderInOutlet("before-sidebar-sections", CustomHeaderTopicButton);
+  /*
   document.addEventListener("DOMContentLoaded", function() {
     const newTopicBtn = document.getElementById("new-topic-button");
     const sidebar = document.getElementById("d-sidebar");
@@ -11,4 +11,5 @@ export default apiInitializer((api) => {
       sidebar.insertBefore(newTopicBtn, sidebar.firstChild);
     }
   });
+  */
 });
